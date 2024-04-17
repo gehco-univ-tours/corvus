4a325742302c2e63c44095f5474f713efb883cc6
Louis Maniere <maniere>
2024-04-17 14:54:30 +0200

    ui raw data

--- R/mod_raw_data.R
+++ R/mod_raw_data.R
@@ -4,28 +4,48 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
+#' @importFrom plotly plotlyOutput
 mod_raw_data_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    fluidPage(
+      fluidRow(
+        plotlyOutput(ns("plot"))
+      ),
+      fluidRow(
+        column(
+          width = 3,
+          selectInput(inputId = ns("station"),
+                      label = "Stations",
+                      choices = c("BE", "PI"))
+        ),
+        column(
+          width = 3,
+          selectInput(inputId = ns("parameter"),
+                      label = "Parameter",
+                      choices = c("Level", "Turbidity"))
+        )
+      )
+    )
+
   )
 }
-    
+
 #' raw_data Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_raw_data_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_raw_data_ui("raw_data_1")
-    
+
 ## To be copied in the server
 # mod_raw_data_server("raw_data_1")
