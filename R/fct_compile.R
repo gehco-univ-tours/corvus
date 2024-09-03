bf2bfc2eb60407805f32ae3c3cd0c163510b6a49
Louis Maniere <maniere>
2024-09-03 16:30:07 +0200

    add download function GB data from FTPS

--- R/fct_compile.R
+++ R/fct_compile.R
@@ -70,3 +70,65 @@ compile_raw <- function(con, station, parameter, sensor){
 
   return(glue::glue("measurement table updated for {toupper(station)} station with {parameter} measures with {rows_affected} rows inserted"))
 }
+
+#' Compile raw data into database
+#'
+#' @importFrom curl curl_download new_handle
+#'
+#' @return string: message
+#' @export
+download_gb <- function(){
+
+  ### FTP DIR ####
+  # FTPS params
+  ftp_username <- Sys.getenv("LOUROUX_FTP_USER")
+  ftp_password <- Sys.getenv("LOUROUX_FTP_PWD")
+  ftp_server <- Sys.getenv("LOUROUX_FTP_SERVER")
+  ftp_port <- Sys.getenv("LOUROUX_FTP_PORT")
+  ftp_data_dir <- "/louroux/GB/"
+
+  # full url with directory
+  url <- paste0("ftps://", ftp_username, ":", ftp_password, "@", ftp_server, ":", ftp_port, ftp_data_dir)  # Directory path you want to list
+
+  # Construct the curl command
+  curl_command <- sprintf('curl.exe -k --ftp-ssl --max-time 2 %s',url)
+
+  # Run the curl command with system
+  output <- system(curl_command, intern = TRUE)
+
+  # list all the csv files in the ftp directory
+  ftp_data_files <- unlist(regmatches(output, gregexpr("\\b\\w+\\.CSV\\b", output)))
+
+  ### LOCAL DIR ####
+
+  # list all the ISTO csv files in local directory
+  local_data_dir <- system.file("ext_data", "GB", package = "louroux")
+  local_data_files <- list.files(path = local_data_dir, pattern = "*.CSV",
+                                 full.names = FALSE)
+  ### DOWNLOAD ####
+
+  # get all the file names in FTP not already in local
+  ftp_data_files_dl <- setdiff(ftp_data_files, local_data_files)
+
+  print(paste0(as.character(length(ftp_data_files_dl)), " files to download."))
+
+  # download the files not in local
+  downloaded_files <- c()
+  for (file in ftp_data_files_dl){
+    download_url <- paste0(url, file)
+    print(download_url)
+    print(file.path(local_data_dir, file))
+    tryCatch({
+      curl_download(download_url, file.path(local_data_dir, file),
+                    handle = new_handle(ssl_verifypeer = FALSE, ssl_verifyhost = FALSE, ftp_use_epsv = FALSE))
+      downloaded_files <- c(downloaded_files, file)
+      print(downloaded_files)
+      print(paste0("Success ext_data/GB/", file, " downloaded."))
+    }, error = function(e) {
+      print(paste0("Error ext_data/GB/", file, ", retry later."))
+    })
+  }
+  return(paste0("Downloaded files: ", downloaded_files))
+}
+
+
