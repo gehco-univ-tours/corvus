6a8931712e7f0f909bd279a3764d6b0b5d2ee8c3
Louis Maniere <louismaniere@orange.fr>
2026-03-23 19:01:19 +0100

    updade sql data

--- inst/sql/db_model_pg_timescale.sql
+++ inst/sql/db_model_pg_timescale.sql
@@ -35,16 +35,16 @@ CREATE TABLE correction_type (
 );
 
 CREATE TABLE measurement (
-    timestamp TIMESTAMPTZ NOT NULL,
+    ts TIMESTAMPTZ NOT NULL,
     sensor_id INTEGER NOT NULL REFERENCES sensor(id),
     value DOUBLE PRECISION,
     value_corr DOUBLE PRECISION,
-    CONSTRAINT measurement_pkey PRIMARY KEY (timestamp, sensor_id)
+    CONSTRAINT measurement_pkey PRIMARY KEY (ts, sensor_id)
 );
 
-SELECT create_hypertable('measurement', by_range('timestamp'));
+SELECT create_hypertable('measurement', by_range('ts'));
 
-CREATE UNIQUE INDEX idx_measurement_timestamp_sensor_id ON measurement(timestamp, sensor_id);
+CREATE UNIQUE INDEX idx_measurement_ts_sensor_id ON measurement(ts, sensor_id);
 
 CREATE INDEX idx_measurement_sensor_id ON measurement(sensor_id);
 
@@ -52,8 +52,8 @@ CREATE TABLE correction (
     id SERIAL PRIMARY KEY,
     sensor_id INTEGER NOT NULL REFERENCES sensor(id),
     author_id INTEGER NOT NULL REFERENCES author(id),
-    timestamp_start TIMESTAMPTZ NOT NULL,
-    timestamp_end TIMESTAMPTZ NOT NULL,
+    ts_start TIMESTAMPTZ NOT NULL,
+    ts_end TIMESTAMPTZ NOT NULL,
     correction_type INTEGER NOT NULL REFERENCES correction_type(id),
     value DOUBLE PRECISION NOT NULL,
     comment TEXT
@@ -62,24 +62,59 @@ CREATE TABLE correction (
 INSERT INTO correction_type (name) VALUES ('Offset'), ('Drift'), ('Delete'), ('Interpolation');
 
 CREATE TABLE field (
-    timestamp TIMESTAMPTZ NOT NULL,
+    ts TIMESTAMPTZ NOT NULL,
     author_id INTEGER NOT NULL REFERENCES author(id),
     station_id INTEGER NOT NULL REFERENCES station(id),
     comment TEXT,
-    CONSTRAINT field_pkey PRIMARY KEY (timestamp, station_id)
+    CONSTRAINT field_pkey PRIMARY KEY (ts, station_id)
 );
 
 -- Add view (optional)
 
+-- hourly materialized view
+CREATE MATERIALIZED VIEW measurement_hourly
+WITH (timescaledb.continuous) AS
+SELECT
+	time_bucket(INTERVAL '1 hour', ts) AS ts,
+	sensor_id,
+	AVG(value) AS value,
+	AVG(value_corr) AS value_corr
+FROM measurement
+GROUP BY sensor_id, time_bucket(INTERVAL '1 hour', ts);
+
+-- Refresh the materialized view every 1 hour
+SELECT add_continuous_aggregate_policy('measurement_hourly',
+  start_offset => NULL,
+  end_offset => INTERVAL '1 h',
+  schedule_interval => INTERVAL '1 h');
+
+-- daily materialized view
+CREATE MATERIALIZED VIEW measurement_daily
+WITH (timescaledb.continuous) AS
+SELECT
+	time_bucket(INTERVAL '1 day', ts) AS ts,
+	sensor_id,
+	AVG(value) AS value,
+	AVG(value_corr) AS value_corr
+FROM measurement
+GROUP BY sensor_id, time_bucket(INTERVAL '1 day', ts);
+
+-- Refresh the materialized view every 1 day
+SELECT add_continuous_aggregate_policy('measurement_daily',
+  start_offset => NULL,
+  end_offset => INTERVAL '1 day',
+  schedule_interval => INTERVAL '1 day');
+
+-- standard view
 CREATE VIEW measurement_data AS (
     SELECT
         station.code AS station,
         parameter.name AS parameter,
-        measurement.timestamp AS timestamp,
+        measurement.ts AS ts,
         measurement.value AS value,
         measurement.value_corr AS value_corr
     FROM measurement
     JOIN sensor ON measurement.sensor_id = sensor.id
     JOIN parameter ON sensor.parameter_id = parameter.id
     JOIN station ON sensor.station_id = station.id)
-    ORDER BY timestamp ASC;
+    ORDER BY ts ASC;
